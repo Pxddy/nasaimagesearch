2ee87108c80a971f871db98d32318b22e0b0c074
Pxddy <patrick.henke@t-online.de>
2023-01-28 12:56:43 +0100

    - bump dependencies - enable renovate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,9 +88,13 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.0.0-beta02")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.activity:activity-compose:1.5.1")
     testImplementation("junit:junit:4.13.2")
