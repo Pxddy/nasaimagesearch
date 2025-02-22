09c0873f4dccd38c5eb295e8c2b08d23f83783e3
Pxddy <patrick.henke@t-online.de>
2025-02-22 11:25:29 +0100

    val agpVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ buildscript {
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.8.1" apply false
-    id("com.android.library") version "8.8.1" apply false
+    val agpVersion = "8.8.1"
+
+    id("com.android.application") version agpVersion apply false
+    id("com.android.library") version agpVersion apply false
     id("com.google.dagger.hilt.android") version "2.55" apply false
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
